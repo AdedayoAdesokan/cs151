ad81671b546fadb02bb51c293ff1381e8338ae09
Adedayo Adesokan <dayoadesokan8@gmail.com>
2020-04-08 16:41:23 -0700

    lab3
    Resubmitting lab 3 properly.

---
+++ lab3/actions1/ActionTester.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ActionTester
+{
+   public static void main(String[] args)
+   {
+      JFrame frame = new JFrame();
+
+      final int FIELD_WIDTH = 20;
+      JTextField textField = new JTextField(FIELD_WIDTH);
+      textField.setText("Click a button!");
+
+      JButton helloButton = new JButton("Say Hello");
+
+      helloButton.addActionListener(event ->
+         textField.setText("Hello, World!"));
+    
+      JButton goodbyeButton = new JButton("Say Goodbye");
+
+      goodbyeButton.addActionListener(event ->
+         textField.setText("Goodbye, World!"));
+      goodbyeButton.setEnabled(false);
+
+      frame.setLayout(new FlowLayout());
+
+      frame.add(helloButton);
+      frame.add(goodbyeButton);
+      frame.add(textField);
+
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.pack();
+      frame.setVisible(true);
+      
+
+      while (helloButton.isEnabled())
+      {
+    	  helloButton.addActionListener(event ->
+          helloButton.setEnabled(false));
+    	  helloButton.addActionListener(event ->
+          goodbyeButton.setEnabled(true));
+    	  if (!helloButton.isEnabled())
+          {
+              goodbyeButton.addActionListener(event ->
+              helloButton.setEnabled(true));
+              goodbyeButton.addActionListener(event ->
+              goodbyeButton.setEnabled(false));
+          }
+      } 
+   }
+}
---
+++ lab3/animation2/AnimationTester.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+/**
+   This program implements an animation that moves
+   a car shape.
+*/
+public class AnimationTester
+{
+   static int x = 0;
+   static int y = 0;
+	public static void main(String[] args)
+   {
+      ArrayList<MoveableShape> shapes = new ArrayList<>();
+      JFrame frame = new JFrame();
+
+      //MoveableShape shape = new CarShape(0, 0, CAR_WIDTH);
+      //MoveableShape shape2 = new CarShape(0, 10, CAR_WIDTH);
+      MoveableShape shape3 = new BouncingBall(0, 0, 150);
+      
+      //shapes.add(shape);
+      //shapes.add(shape2);
+      shapes.add(shape3);
+      
+      for (MoveableShape newShape : shapes)
+      {
+    	  ShapeIcon icon = new ShapeIcon(newShape,
+    	            ICON_WIDTH, ICON_HEIGHT);
+
+    	      JLabel label = new JLabel(icon);
+    	      frame.setLayout(new FlowLayout());
+    	      frame.add(label);
+
+    	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	      frame.pack();
+    	      frame.setVisible(true);
+
+    	      final int DELAY = 100;
+    	      // Milliseconds between timer ticks
+    	      Timer t = new Timer(DELAY, event ->
+    	         {
+    	            newShape.move();
+    	            label.repaint();
+    	         });
+    	      t.start();
+      }
+     
+   }
+
+   private static final int ICON_WIDTH = 1000;
+   private static final int ICON_HEIGHT = 1000;
+   private static final int CAR_WIDTH = 100;
+}
