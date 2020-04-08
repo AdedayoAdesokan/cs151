5126d411e8116ec8deeea218f5a1c5325f04c60b
Adedayo Adesokan <dayoadesokan8@gmail.com>
2020-04-08 16:20:59 -0700

    lab2
    Resubmitting lab 2 properly

---
+++ lab2/Tester.java
@@ -0,0 +1,59 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Tester 
+{
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+
+    @Test
+    void test1() {
+        // fail("Not yet implemented");
+        int[] a = new int[] { 5, 4, 1, 9, 2, 6 };
+        Arrays.swapLargestAndSmallest(a);
+        int[] aSwapped = new int[] { 5, 4, 9, 1, 2, 6 };
+        assertArrayEquals(aSwapped, a);
+    }
+
+    @Test
+    void test2() {
+        int[] a = new int[] { 1, 4, 1, 9, 2, 6 };
+        Arrays.swapLargestAndSmallest(a);
+        int[] aSwapped = new int[] { 9, 4, 1, 1, 2, 6 };
+        assertArrayEquals(aSwapped, a);
+    }
+
+    @Test
+    void test3() {
+        int[] a = new int[] { 1, 4, 2, 9, 9, 6 };
+        Arrays.swapLargestAndSmallest(a);
+        int[] aSwapped = new int[] { 9, 4, 2, 1, 9, 6 };
+        assertArrayEquals(aSwapped, a);
+    }
+
+    @Test
+    void test4() {
+        int[] a = new int[] { 1 };
+        Arrays.swapLargestAndSmallest(a);
+        int[] aSwapped = new int[] { 1 };
+        assertArrayEquals(aSwapped, a);
+    }
+}
